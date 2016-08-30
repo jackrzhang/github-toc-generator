537402569f3751751610039a2435bc9fd381568b
Jack Zhang <15jzhang@gmail.com>
2016-08-30 01:21:53 -0400

    Add usage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 
-// import program from 'commander';
+import program from 'commander';
+import pkg from './../package.json';
 // import chalk from 'chalk';
 
-console.log('Hello, world!');
+program
+  .version(pkg.version)
+  .usage('[options] <user> <repository>')
+  .description('A command-line utility that generates Tables of Contents for Github READMEs.')
+  .option('-d, --depth <number>', 'specifiy the maximum header depth (1 - 6) of the TOC')
+  .option('-o, --open', 'open the README in browser')
+  .parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
+} else {
+  
+}
